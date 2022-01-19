8c15003c3ebea6f0ffe6d943cf82c8fe68e82db2
Taha <tzengin@yozu.co.uk>
2022-01-19 14:07:06 +0000

    further config for eslint

--- App.js
+++ App.js
@@ -2,53 +2,14 @@ import React, {useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
-  View,
   TouchableOpacity,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer} testID="debug">
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
   const [value, setValue] = useState(0);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
   const handleIncrease = () => {
     setValue(prev => ++prev);
   };
@@ -58,57 +19,21 @@ const App = () => {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <Section>
-            <TouchableOpacity style={styles.tapMe} onPress={handleIncrease}>
-              <Text>Increase</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.tapMe} onPress={handleDecrease}>
-              <Text>Decrease</Text>
-            </TouchableOpacity>
-          </Section>
-
-          <Section>
-            <Text>Current Value is : {value}</Text>
-          </Section>
-        </View>
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <TouchableOpacity style={styles.tapMe} onPress={handleIncrease}>
+          <Text>Increase</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tapMe} onPress={handleDecrease}>
+          <Text>Decrease</Text>
+        </TouchableOpacity>
+        <Text>Current Value is : {value}</Text>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
   tapMe: {
     marginLeft: 10,
     width: 100,
