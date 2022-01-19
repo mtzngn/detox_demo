39c874e7d64c4a6244bfeb34ee64b96693053845
Taha <tzengin@yozu.co.uk>
2022-01-19 13:54:52 +0000

    more tests!

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -9,7 +9,32 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
 
-  it('increase button', async () => {
+  it('increase button should be visible', async () => {
     await expect(element(by.text('Increase'))).toBeVisible();
+  });
+
+  it('decrease button should be visible', async () => {
+    await expect(element(by.text('Decrease'))).toBeVisible();
+  });
+
+  it('should allow to increase', async () => {
+    await element(by.text('Increase')).tap();
+    await element(by.text('Increase')).tap();
+    await element(by.text('Increase')).tap();
+    await expect(element(by.text('Current Value is : 3'))).toBeVisible();
+  });
+
+  it('should allow to decrase', async () => {
+    await element(by.text('Decrease')).tap();
+    await element(by.text('Decrease')).tap();
+    await element(by.text('Decrease')).tap();
+    await expect(element(by.text('Current Value is : -3'))).toBeVisible();
+  });
+
+  it('should allow for increase than decrease', async () => {
+    await element(by.text('Increase')).tap();
+    await element(by.text('Increase')).tap();
+    await element(by.text('Decrease')).tap();
+    await expect(element(by.text('Current Value is : 1'))).toBeVisible();
   });
 });
