35a015be4203868bb053e7168a5cb4de7801ea35
Taha <tzengin@yozu.co.uk>
2022-03-01 10:59:30 +0000

    react navigation v6 removed

--- App.js
+++ App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import WelcomePage from './src/Pages/WelcomePage/WelcomePage';
 import UserPage from './src/Pages/UserPage/UserPage';
 import TestPage from './src/Pages/TestPage/TestPage';
@@ -10,41 +7,8 @@ import SettingsPage from './src/Pages/SettingsPage/SettingsPage';
 import FeedPage from './src/Pages/FeedPage/FeedPage';
 import {Text} from 'react-native';
 
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-function LogoTitle() {
-  return <Text>LOGO TITLE</Text>;
-}
-function Home() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="TestPage" component={TestPage} />
-      <Tab.Screen name="ProfilePage" component={ProfilePage} />
-      <Tab.Screen name="SettingsPage" component={SettingsPage} />
-      <Tab.Screen name="FeedPage" component={FeedPage} />
-    </Tab.Navigator>
-  );
-}
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen name="Welcome" component={WelcomePage} />
-        <Stack.Screen
-          name="UserPage"
-          component={UserPage}
-          options={{
-            headerTitle: props => <LogoTitle {...props} />,
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <WelcomePage />;
 };
 
 export default App;
