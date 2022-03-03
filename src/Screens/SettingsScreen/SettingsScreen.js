f34251b633c7fa54f651a8fbb17ca73c8004e573
Taha <tzengin@yozu.co.uk>
2022-03-03 11:02:21 +0000

    icons succesfully added and a small form created to be tested

--- src/Screens/SettingsScreen/SettingsScreen.js
+++ src/Screens/SettingsScreen/SettingsScreen.js
@@ -1,13 +1,67 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const SettingScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Settings Screen</Text>
-    </View>
-  );
-};
+class SettingScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: '', password: '', submitted: false};
+
+    this.onNameChange = this.onNameChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  onNameChange(text) {
+    this.setState({name: text});
+  }
+  onPasswordChange(text) {
+    this.setState({password: text});
+  }
+  handleSubmit() {
+    this.setState({submitted: true});
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Settings Screen</Text>
+
+        {!this.state.submitted ? (
+          <SafeAreaView style={styles.form}>
+            <TextInput
+              style={styles.input}
+              onChangeText={text => this.onNameChange(text)}
+              value={this.state.name}
+              placeholder="Name"
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={text => this.onPasswordChange(text)}
+              value={this.state.password}
+              placeholder="Password"
+            />
+            <TouchableOpacity
+              style={styles.submit}
+              onPress={() => this.handleSubmit()}>
+              <Text>Submit</Text>
+            </TouchableOpacity>
+          </SafeAreaView>
+        ) : (
+          <SafeAreaView style={styles.submitted}>
+            <Text>Thank you for updating!</Text>
+            <Icon name="rocket" size={30} color="#000" style={styles.icon} />
+          </SafeAreaView>
+        )}
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -15,6 +69,32 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'lightbrown',
+  },
+  input: {
+    backgroundColor: 'lightblue',
+    height: 40,
+    width: 200,
+    margin: 10,
+    padding: 10,
+    borderRadius: 5,
+  },
+  form: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  submit: {
+    margin: 10,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: 'lightgreen',
+  },
+  submitted: {
+    display: 'flex',
+    alignItems: 'center',
+    margin: 50,
+  },
+  icon: {
+    margin: 20,
   },
 });
 
--- src/Screens/TestScreen/TestScreen.js
+++ src/Screens/TestScreen/TestScreen.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 class TestScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Test Screen</Text>
-        <Button
-          title="Go to Test Screen"
-          onPress={() => this.props.navigation.navigate('Welcome')}
-        />
       </View>
     );
   }
